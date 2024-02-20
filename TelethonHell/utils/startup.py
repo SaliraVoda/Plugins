1bbfce580ef9967ba2be9ec3de4cd33b9bfc99e1
MadMax! <159709370+MadMax393@users.noreply.github.com>
2024-02-20 18:23:00 +0530

    Update startup.py

--- TelethonHell/utils/startup.py
+++ TelethonHell/utils/startup.py
@@ -14,7 +14,7 @@ async def logger_id(client):
     try:
         grp = await client(
             functions.channels.CreateChannelRequest(
-                title="HELLEXTENDED LOGGER ⚡", about=desc, megagroup=True
+                title="EXTENDED LOGS ⚡", about=desc, megagroup=True
             )
         )
         grp_id = grp.chats[0].id

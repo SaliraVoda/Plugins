1227209b5f2f6bba4f65db94cdd93831e66b81e4
MadMax! <159709370+MadMax393@users.noreply.github.com>
2024-02-20 19:26:55 +0530

    Update alive.py

--- TelethonHell/plugins/alive.py
+++ TelethonHell/plugins/alive.py
@@ -24,7 +24,7 @@ ALIVE_TEMP = """
 """
 
 msg = """{}\n
-<b><i>🏅𝙷𝙴𝙻𝙻𝙱𝙾𝚃 𝙴𝚇𝚃𝙴𝙽𝙳𝙴𝙳 𝚂𝚃𝙰𝚃𝚄𝚂🏅</b></i>
+<b><i>🌱𝐁𝐎𝐓 𝐒𝐓𝐀𝐓𝐔𝐒🌱</b></i>
 <b>Telethon ≈</b>  <i>{}</i>
 <b>Extended ≈</b>  <i>{}</i>
 <b>Uptime ≈</b>  <i>{}</i>

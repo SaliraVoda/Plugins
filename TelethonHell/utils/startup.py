3a81ee8745294b85f41d0961e6fb03928f3be4fb
MadMax! <159709370+MadMax393@users.noreply.github.com>
2024-02-20 19:50:41 +0530

    Update startup.py

--- TelethonHell/utils/startup.py
+++ TelethonHell/utils/startup.py
@@ -83,14 +83,14 @@ async def start_msg(client, pic, version, total):
 <b><i>Sudo:</b></i> <code>{is_sudo}</code>
 <b><i>Library:</b></i> <code>Telethon - {__telever__}</code>
 
-<b><i>»» <u><a href='https://t.me/hellbot_extended'>†hê Hêllẞø†</a></u> ««</i></b>
+<b><i>»» <u><a href='https://t.me/hellbot_extended'>ℍ𝕖𝕝𝕝𝕓𝕠𝕥 𝔼𝕩𝕥𝕖𝕟𝕕𝕖𝕕</a></u> ««</i></b>
 """
     await client.send_file(
         Config.LOGGER_ID,
         pic,
         caption=text,
         parse_mode="HTML",
-        buttons=[[Button.url("HellBot Network", "https://t.me/HellBot_Extended")]],
+        buttons=[[Button.url("HellBot Extended", "https://t.me/HellBot_Extended")]],
     )
 
 

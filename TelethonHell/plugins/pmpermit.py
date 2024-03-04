0eb8f34c3b30854d660ad2643fc773e0e127c9ac
SaliraVoda <161962941+SaliraVoda@users.noreply.github.com>
2024-03-04 21:03:34 +0530

    Update pmpermit.py

--- TelethonHell/plugins/pmpermit.py
+++ TelethonHell/plugins/pmpermit.py
@@ -707,7 +707,7 @@ async def do_pm_permit_action(chat_ids, event, client):
                 )
     firstname = getuser.users[0].first_name
     CSTM_PMP = gvarstat("CUSTOM_PMPERMIT") or None
-    thee_message = f"🔰 𝐄𝐱𝐭𝐞𝐧𝐝𝐞𝐝 𝐏𝐌 𝐒𝐞𝐜𝐮𝐫𝐢𝐭𝐲\n\n <code>👋 Hello [{firstname}](tg://user?id={reply.sender_id}), This is an automated message and you are requested not to spam here. Please wait until my Master approves you to pm else you'll be blocked automatically!!</code>"
+    thee_message = f"🔰 𝐄𝐱𝐭𝐞𝐧𝐝𝐞𝐝 𝐏𝐌 𝐒𝐞𝐜𝐮𝐫𝐢𝐭𝐲\n\n <code>👋 Hello [{firstname}](tg://user?id={event.chat_id}), This is an automated message and you are requested not to spam here. Please wait until my Master approves you to pm else you'll be blocked automatically!!</code>"
     if CSTM_PMP:
         thee_message = f"🔰 𝐄𝐱𝐭𝐞𝐧𝐝𝐞𝐝 𝐏𝐌 𝐒𝐞𝐜𝐮𝐫𝐢𝐭𝐲\n\n <code>{CSTM_PMP}</code>"
     PIC = gvarstat("PMPERMIT_PIC") or "https://te.legra.ph/file/f667c4c121294c863e068.jpg"
